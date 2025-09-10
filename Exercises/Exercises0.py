b8e8c7ff71dafa2e069d58d1880ce4d1a522faf5
Wilgot Lucaci <wilgot@lucaci.se>
2025-09-10 14:29:51 +0200

    uppgift2

---
+++ Exercises/Exercises0.py
@@ -0,0 +1,23 @@
+#Uppgift 1
+
+import math
+a = 3
+b = 4
+c = math.sqrt(a**2 + b**2)
+print(c)
+
+a = 5
+c = 7
+b = math.sqrt(c**2 - a**2)
+
+print(b)
+
+#Uppgift 2
+
+predictions = 356
+correct_predictions = 300
+
+accuracy = correct_predictions / predictions
+print(accuracy)
+
+#Uppgift 3
