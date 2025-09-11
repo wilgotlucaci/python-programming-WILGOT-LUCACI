fc78e7e38b079e0ea2b4c4375498fe6ec40f3ed5
wilgotlucaci <wilgot.lucaci@iths.se>
2025-09-11 11:06:55 +0200

    klar

--- Exercises/Exercises0.py
+++ Exercises/Exercises0.py
@@ -22,6 +22,15 @@ print(accuracy)
 
 #Uppgift 3
 
+TP = 2
+FP = 2
+FN = 11
+TN = 985
+
+accuracy = (TP + TN) / (TP + TN + FP + FN)
+print(accuracy)
+
+
 #Uppgift 4
 
 x1 = 4
@@ -34,3 +43,29 @@ y = k + y1
 m = y1 - k*x1
 print(k)
 print(m)
+
+
+#Uppgift 5
+
+x1 = 3
+y1 = 5
+x2 = -2
+y2 = 4
+
+distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+print(distance)
+
+#Uppgift 6
+
+p1 = 2
+p2 = 1
+p3 = 4
+q1 = 3
+q2 = 1
+q3 = 0
+
+p = (p1, p2, p3)
+q = (q1, q2, q3)
+
+distance = math.sqrt((q1 - p1)**2 + (q2 - p2)**2 + (q3 - p3)**2)
+print(distance)
