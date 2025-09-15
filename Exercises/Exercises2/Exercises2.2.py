7df782cf4647684960b6836dca514a2b069198e6
wilgotlucaci <wilgot.lucaci@iths.se>
2025-09-15 16:33:11 +0200

    uppgift

---
+++ Exercises/Exercises2/Exercises2.2.py
@@ -0,0 +1,17 @@
+#Uppgift A
+
+i = 1
+total = 0
+
+while i <= 100:
+    total += i
+    i += 1
+    print(f"The sum between all numbers from 1-100 is {total}")
+
+#Uppgift B
+k = 1
+sum = 0
+while k <= 100:
+    sum += k
+    k += 2
+    print(f"The sum of the numbers between 1-100 with an increment of 2 is {sum} ")
