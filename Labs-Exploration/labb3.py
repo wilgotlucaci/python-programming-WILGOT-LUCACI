4aa447b9507eb0161c3c6a65fe1c1bdb1f857aa8
wilgotlucaci <wilgot.lucaci@iths.se>
2025-09-24 11:55:35 +0200

    labb3 start

---
+++ Labs-Exploration/labb3.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+path1 = "/Users/wilgotlucaci/Desktop/Labb3/unlabelled_data.csv"
+
+with open(path1, "r") as un:
+    unlabelled = un.read()
+
+
+plt.xticks
+plt.show
+
+
